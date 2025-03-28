981edaab826c57bf863407f32a1e8b9d4f2b74ed
antidalai <dalaibiju1@gmail.com>
2025-03-28 14:15:02 -0400

    Finished excel inputting and outputting incorporation

--- annuity-projection.py
+++ annuity-projection.py
@@ -1,3 +1,4 @@
+import pandas as pd
 # Creating list of variables (will be inputs from excel sheet eventually)
 # Slightly unnessary since variables could be declared when gathering values from excel
 
@@ -8,6 +9,24 @@ years = 2 # Number of Years annuity will be paid out
 payment = 0 # How much will be paid each period
 
 paymentType = "Due" # Will either be Ordinary (payments at end) or Due (payments at beginning)
+
+
+# 1. Read input values from "Inputs" sheet
+df_inputs = pd.read_excel(r"C:\Users\dalai\Actuarial\fixed-annuity-projection\AnnuityModel.xlsx", sheet_name="Sheet1")
+input_data = pd.Series(df_inputs.iloc[:,1].values, index=df_inputs.iloc[:,0]).to_dict()
+
+
+print(input_data)
+
+
+# Extract inputs\
+principal = input_data["Principal"]
+annual_rate = input_data["Interest Rate"]
+years = input_data["Years"]
+payments_per_year = input_data["Frequency"]
+payment_type = input_data["Payment Type"]
+
+
 
 periodic_interest = interest / frequency
 total_periods = frequency * years
@@ -47,3 +66,10 @@ for i in range(total_periods):
 for row in table:
     print("Period: " , row[0], " Principal: " , row[1], " Interest Collected: ", row[2], " Payment: ", row[3], " Balance: ", row[4])
 
+
+# 5. Convert table to DataFrame
+df_output = pd.DataFrame(table, columns=["Period", "Begin Balance", "Interest", "Payment", "End Balance"])
+
+# 6. Write to same Excel file, different sheet
+with pd.ExcelWriter(r"C:\Users\dalai\Actuarial\fixed-annuity-projection\AnnuityModel.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+    df_output.to_excel(writer, sheet_name="Projection", index=False)
